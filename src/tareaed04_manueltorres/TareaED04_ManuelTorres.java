d4563323d421cf7061b9ac2fb8e2c3ecbc7aa858
Manuel <Manuel@Msito>
2020-02-24 19:39:35 +0100

    Utiliza las funciones de refactorización  de código, utilizando Eclipse y Netbeans, para que las variables y constantes de tu código tengan nombres significativos. Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio.

--- src/tareaed04_manueltorres/TareaED04_ManuelTorres.java
+++ src/tareaed04_manueltorres/TareaED04_ManuelTorres.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class TareaED04_ManuelTorres {
 
     public static void main(String[] args) {
-//las variables a y b servirán para guardar el resultado de la tirada de dos monedas
-        int a, b;
+//las variables monedaUno y monedaDos servirán para guardar el resultado de la tirada de dos monedas
+        int monedaUno, monedaDos;
         int contadorCaras = 0, contadorCruces = 0, numTiradas;
 //las variables contador1 y contador2 servirán para guardar el número de caras y cruces dobles consecutivas
-        int cont1 = 0, cont2 = 0;
+        int caraDobleConsecutiva = 0, cruzDobleConsecutiva = 0;
         int maximoCarasDoblesConsecutivas = 0, maximoCrucesDoblesConsecutivas = 0;
         boolean esCruzDoble = false, esCaraDoble = false;
         String tirada1;
@@ -22,17 +22,17 @@ public class TareaED04_ManuelTorres {
         } while (numTiradas < 1 || numTiradas > 50);
 
         for (int i = 0; i < numTiradas; i++) {
-            a = (int) (Math.random() * 2);
-            b = (int) (Math.random() * 2);
+            monedaUno = (int) (Math.random() * 2);
+            monedaDos = (int) (Math.random() * 2);
 
-            if (a == 0) {
+            if (monedaUno == 0) {
                 contadorCaras++;
                 tirada1 = "cara";
             } else {
                 contadorCruces++;
                 tirada1 = "cruz";
             }
-            if (b == 0) {
+            if (monedaDos == 0) {
                 contadorCaras++;
                 tirada2 = "cara";
             } else {
@@ -41,8 +41,8 @@ public class TareaED04_ManuelTorres {
             }
             System.out.println("Moneda 1: " + tirada1 + ", Moneda 2: " + tirada2);
 
-            if (a == b) {
-                if (a == 0) {
+            if (monedaUno == monedaDos) {
+                if (monedaUno == 0) {
                     esCaraDoble = true;
                     esCruzDoble = false;
                 } else {
@@ -55,21 +55,21 @@ public class TareaED04_ManuelTorres {
             }
 
             if (esCaraDoble) {
-                cont1++;
-                if (cont1 > maximoCarasDoblesConsecutivas) {
-                    maximoCarasDoblesConsecutivas = cont1;
+                caraDobleConsecutiva++;
+                if (caraDobleConsecutiva > maximoCarasDoblesConsecutivas) {
+                    maximoCarasDoblesConsecutivas = caraDobleConsecutiva;
                 }
             } else {
-                cont1 = 0;
+                caraDobleConsecutiva = 0;
             }
 
             if (esCruzDoble) {
-                cont2++;
-                if (cont2 > maximoCrucesDoblesConsecutivas) {
-                    maximoCrucesDoblesConsecutivas = cont2;
+                cruzDobleConsecutiva++;
+                if (cruzDobleConsecutiva > maximoCrucesDoblesConsecutivas) {
+                    maximoCrucesDoblesConsecutivas = cruzDobleConsecutiva;
                 }
             } else {
-                cont2 = 0;
+                cruzDobleConsecutiva = 0;
             }
         }
 
